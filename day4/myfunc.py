11ab5a8afe7740620b0f001177a7ddd6c7e7614d
k5-mot-WSL2 <k5.mot.888@gmail.com>
2021-06-02 17:17:55 +0900

    Revise kadai for day4.

--- day4/myfunc.py
+++ day4/myfunc.py
@@ -1,19 +1,45 @@
-def Differential(TargetFunc, x): 
-  h = 1e-4 
-  return (TargetFunc(x+h) - TargetFunc(x))/h 
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
-def NewtonSolver(TargetFunc, x, n_loop = 10000, error_limit = 0.001): 
-  for n in range(n_loop): 
-    try: 
-      epsilon = TargetFunc(x)/Differential(TargetFunc, x) 
-    except ZeroDivisionError: 
-      x = 'stopped' 
-      break 
-    else: 
-      x_next = x - epsilon 
-      if (x_next - x)**2 < error_limit: 
-        break 
-      x = x_next 
-    finally: 
-      pass 
+def solve_sqrt(num):
+  x = num
+  n_loop = 10000
+  error_limit = 0.001
+  for n in range(n_loop):
+    try:
+      epsilon = (x**2 - num) / (2 * x)
+    except ZeroDivisionError:
+      x = 'stopped'
+      break
+    else:
+      x_next = x - epsilon
+      if (x_next - x)**2 < error_limit:
+        break
+      x = x_next
+    finally:
+      pass
+  # print(x)
+
+  return x
+
+
+def Differential(TargetFunc, x):
+  h = 1e-4
+  return (TargetFunc(x + h) - TargetFunc(x)) / h
+
+
+def NewtonSolver(TargetFunc, x, n_loop=10000, error_limit=0.001):
+  for n in range(n_loop):
+    try:
+      epsilon = TargetFunc(x) / Differential(TargetFunc, x)
+    except ZeroDivisionError:
+      x = 'stopped'
+      break
+    else:
+      x_next = x - epsilon
+      if (x_next - x)**2 < error_limit:
+        break
+      x = x_next
+    finally:
+      pass
   return x
---
+++ day4/solve_sqrt5.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import myfunc
+
+print('kadai4-1')
+print()
+
+print('√5 = ' + str(myfunc.solve_sqrt(5)))

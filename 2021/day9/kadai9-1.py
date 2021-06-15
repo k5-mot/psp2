fcc9594e2a4efbe23bc2f69122adcf9df4a8bc05
k5-mot-WSL2 <k5.mot.888@gmail.com>
2021-06-16 01:57:34 +0900

    Add day9.

---
+++ 2021/day9/kadai9-1.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import sys
+import numpy as np
+import math
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import pandas as pd
+
+
+def main():
+
+  # Prepare data
+  t = np.linspace(-2 * np.pi, 2 * np.pi, int(1e4))
+  f = np.sin(t)
+  # Prepare graph
+  fig, axes = plt.subplots(1, 1)
+  # Setup figure
+  fig.suptitle("Main Title", fontsize=16)
+  # Setup axes.
+  axes.set_title("Graph Title")
+  axes.set_xlabel("t")
+  axes.set_ylabel("f(t)")
+  axes.plot(t, f, color="blue", label="f(t)")
+  axes.grid(b=True, which="both", axis="both", color="black",
+            alpha=0.5, linestyle="-", linewidth=1)
+  # axes.grid(b=True, which="minor", axis="both", color="grey",
+  #          alpha=0.5, linestyle="--", linewidth=1)
+  axes.legend(loc=0, frameon=True)
+
+  # Display graph
+  plt.tight_layout()
+  plt.savefig("images/kadai9-1.png")
+  plt.show()
+
+
+if __name__ == "__main__":
+  sys.exit(main())

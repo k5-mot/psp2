31a68f59e1d19cc8b7831cfe58c75cb1dc88b706
k5-mot-WSL2 <k5.mot.888@gmail.com>
2021-07-01 04:31:43 +0900

    Change HW of day10.

--- 2021/day10/kadai10-2.py
+++ 2021/day10/kadai10-2.py
@@ -2,33 +2,66 @@ import sys
 import numpy as np
 
 
-def learnModel(M, b):
-  Mi = np.linalg.inv(M)
-  a = np.matmul(Mi, b)
-  return a
+def model1(input, term):
+  return np.power(input, term)
 
 
-def inferModel(a, s):
-  return a[0] * (s ** 3) + a[1] * (s ** 2) + a[2] * (s ** 1) + a[3] * s
+def model2(input, term):
+  return np.power(input, -term)
+
+
+def createPattern(size):
+  pattern_in = np.random.randint(-100, 100, size)
+  pattern_out = np.random.randint(-100, 100, size)
+  # pattern_in = np.array([1, 2, 3, 4])
+  # pattern_out = np.array([3, 5, 7, 9])
+  return pattern_in, pattern_out
+
+
+def createMatrix(pattern_in, model):
+  size = len(pattern_in)
+  matrix_in = np.zeros((size, size))
+  for i in range(size):
+    input = pattern_in[i]
+    for j in range(size):
+      matrix_in[i][j] = model(input, size - j - 1)
+  return matrix_in
+
+
+def learnModel(pattern_in, pattern_out, model):
+  matrix_in = createMatrix(pattern_in, model)
+  pattern_inv = np.linalg.inv(matrix_in)
+  params = np.matmul(pattern_inv, pattern_out)
+  return params
+
+
+def inferModel(params, input, model):
+  sum = 0
+  for i in range(len(params)):
+    sum += params[i] * model(input, len(params) - i - 1)
+  return sum
 
 
 def main(argv):
-  M = np.random.randint(0, 100, (4, 4))
-  b = np.random.randint(0, 100, 4)
-  a = learnModel(M, b)
-  c = np.random.randint(0, 100, 1)
-  d = inferModel(a, c)
+  model = model1
+  pattern_in, pattern_out = createPattern(4)
+  params = learnModel(pattern_in, pattern_out, model)
+  input = np.random.randint(0, 100, 1)
+  output = inferModel(params, input, model)
 
-  print('M = ')
-  print(M)
-  print('b = ', end='')
-  print(b)
-  print('a = M^-1 b = ', end='')
-  print(a)
-  print('c = ', end='')
-  print(c[0])
-  print('d = f(a, c) = ', end='')
-  print(d[0])
+  print('pattern1 = ', end='')
+  print(pattern_in)
+  print('pattern2 = ', end='')
+  print(pattern_out)
+  print('matrix1  = ')
+  print(createMatrix(pattern_in, model))
+  print('params   = ', end='')
+  print(params)
+  print()
+  print('input    = ', end='')
+  print(input[0])
+  print('output   = ', end='')
+  print(output[0])
   print()
 
 

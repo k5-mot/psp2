2e2ca2005e8469a9a6838042fe2a76061cbfcedc
k5-mot-WSL2 <k5.mot.888@gmail.com>
2021-07-01 03:16:36 +0900

    Change HW of day10.

--- 2021/day10/kadai10-2.py
+++ 2021/day10/kadai10-2.py
@@ -13,41 +13,23 @@ def inferModel(a, s):
 
 
 def main(argv):
-  M = np.array(
-      [[1, 1, 1, 1],
-       [8, 4, 2, 1],
-       [27, 9, 3, 1],
-       [64, 16, 4, 1]])
-  b = np.array([3, 5, 7, 9])
+  M = np.random.randint(0, 100, (4, 4))
+  b = np.random.randint(0, 100, 4)
   a = learnModel(M, b)
-  d = inferModel(a, 10)
+  c = np.random.randint(0, 100, 1)
+  d = inferModel(a, c)
 
   print('M = ')
   print(M)
-  print('a = ', end='')
-  print(a)
   print('b = ', end='')
   print(b)
-  print('d = ', end='')
-  print(d)
+  print('a = M^-1 b = ', end='')
+  print(a)
+  print('c = ', end='')
+  print(c[0])
+  print('d = f(a, c) = ', end='')
+  print(d[0])
   print()
-  print()
-
-  for i in range(4):
-    M = np.random.randint(0, 100, (4, 4))
-    b = np.random.randint(0, 100, 4)
-    a = learnModel(M, b)
-    d = inferModel(a, 10)
-
-    print('M = ')
-    print(M)
-    print('a = ', end='')
-    print(a)
-    print('b = ', end='')
-    print(b)
-    print('d = ', end='')
-    print(d)
-    print()
 
 
 if __name__ == "__main__":

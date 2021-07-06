34f1cb46d35fff96030ceceb24fee879068af8c0
k5-mot-WSL2 <k5.mot.888@gmail.com>
2021-07-06 23:53:20 +0900

    Add HW of day12.

--- 2021/day12/main.py
+++ 2021/day12/main.py
@@ -7,14 +7,12 @@ import myMontyHall as my
 def main(argv):
   n_trial = 1000
   tactics_list = [0, 1]  # 1ならKeep, 0ならChange
-
   prob = [0, 0]
 
   for tactics in tactics_list:
     p = 0
     for i in range(n_trial):
       p += my.MontyHall(tactics)
-
     prob[tactics] = p / n_trial
 
   print('Keep戦略   : ', prob[1])

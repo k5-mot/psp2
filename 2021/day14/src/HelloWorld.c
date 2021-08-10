f7da597bfee0fa231c96de5e66e8bf0a370f6bc7
k5-mot <k5.mot.888@gmail.com>
2021-08-11 02:59:44 +0900

    Change README.

--- 2021/day14/src/HelloWorld.c
+++ 2021/day14/src/HelloWorld.c
@@ -1,8 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 int main(int argc, char* argv[])
 {
-  printf("Hello, World!!!\n");
+  int i;
+  double x[10];
+  printf("hello world\n");
+  for (i = 0; i < 10; i++)
+  {
+    x[i] = cos(i * M_PI / 3);
+    printf("x[%d] : %f\n", i, x[i]);
+  }
   return EXIT_SUCCESS;  
 }

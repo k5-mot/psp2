10a2637e48b2b866eee1869b193b87e5bd58eb93
k5-mot-cos <k5.mot.888@gmail.com>
2021-08-11 14:03:59 +0900

    Change day5.

--- 2021/day5/kadai5-1.py
+++ 2021/day5/kadai5-1.py
@@ -1,30 +1,12 @@
 #!/usr/bin/env python
 # coding: utf-8
-import sys
 import Rational as Rational
 
-
-def main():
-  x = Rational.Rational(1, 10)
-  print('x = ', end='')
-  print(x)
-
-  y = Rational.Rational(2, 10)
-  print('y = ', end='')
-  print(y)
-
-  print('x + y = ', end='')
-  print(x + y)
-
-  print('x - y = ', end='')
-  print(x - y)
-
-  print('x * y = ', end='')
-  print(x * y)
-
-  print('x / y = ', end='')
-  print(x / y)
-
-
-if __name__ == "__main__":
-  sys.exit(main())
+x = Rational.Rational(1, 10)
+y = Rational.Rational(2, 10)
+print('x     = ', x)
+print('y     = ', y)
+print('x + y = ', x + y)
+print('x - y = ', x - y)
+print('x * y = ', x * y)
+print('x / y = ', x / y)
--- 2021/day5/kadai5-2.py
+++ 2021/day5/kadai5-2.py
@@ -1,19 +1,9 @@
 #!/usr/bin/env python
 # coding: utf-8
-import sys
 import RationalChild as RationalChild
 
-
-def main():
-  x = RationalChild.RationalChild(5, 10)
-  print('x = ', end='')
-  print(x)
-
-  y = 2
-  print('y = ', end='')
-  print(y)
-  print("x ** y = " + str(x ** y))
-
-
-if __name__ == "__main__":
-  sys.exit(main())
+x = RationalChild.RationalChild(5, 10)
+y = 2
+print('x      = ', x)
+print('y      = ', y)
+print('x ** y = ', x ** y)
